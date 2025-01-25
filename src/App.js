4c97c24d63eb90697c34411fb0facefe976e1909
Matheuskauanjg <mkauanpinto@gmail.com>
2025-01-25 15:09:21 +0000

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Home />
     </div>
