bf1afdfb8293ce46f38696445b940e809dac6387
Matheuskauanjg <mkauanpinto@gmail.com>
2025-01-25 11:20:02 -0300

    j

--- school-research-system/src/utils/mockData.js
+++
@@ -1,8 +0,0 @@
-export const mockData = [
-    { id: 1, category: "História", topic: "Revolução Francesa", description: "Um marco da história moderna." },
-    { id: 2, category: "Geografia", topic: "Clima Tropical", description: "Características de regiões tropicais." },
-    { id: 3, category: "Matemática", topic: "Teorema de Pitágoras", description: "Famoso teorema de geometria." },
-    { id: 4, category: "Física", topic: "Lei da Gravitação Universal", description: "Lei de Newton sobre a atração gravitacional." },
-    // Mais dados aqui conforme necessário
-  ];
-  
