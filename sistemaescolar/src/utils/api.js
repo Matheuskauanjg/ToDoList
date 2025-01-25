3c8ea5ecc74ccbc28bb476a00d5a67bd3c96423f
Matheuskauanjg <mkauanpinto@gmail.com>
2025-01-25 14:54:18 +0000

    d

--- sistemaescolar/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-export const buscarTemas = (termo) => {
-    const temas = require('../data/temas.json');
-    return temas.filter((tema) =>
-      tema.nome.toLowerCase().includes(termo.toLowerCase())
-    );
-  };
-  
